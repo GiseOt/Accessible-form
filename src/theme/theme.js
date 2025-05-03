17ac475124c67033b41073ffb789b1f88794665f
Gisella Ortiz de la Tabla <gisellanalia@gmail.com>
2025-05-03 16:26:56 +0200

    feat(form): add pulsing border animation to indicate active speech recognition

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -3,13 +3,13 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
 	palette: {
 		primary: {
-			light: "rgb(166, 241, 224)",
+			light: "#fafafa",
 			main: "rgb(115, 199, 199)",
 			dark: "#002884",
 			contrastText: "#fff",
 		},
 		secondary: {
-			light:"rgb(244, 248, 211)",
+			light: "rgb(227, 227, 224)",
 			main: "rgb(166, 241, 224)",
 			dark: "#ba000d",
 			contrastText: "#000",
